3635b31b3a203fd78f26264a48422c80cda01423
junking <827652549@qq.com>
2020-03-30 16:02:59 +0800

    Initial commit

---
+++ dist/countable.js
@@ -0,0 +1,23 @@
+var defaultOptions = {
+  countable:true
+};
+
+// Docsify plugin functions
+function plugin(hook, vm) {
+  let wordsCount;
+  hook.beforeEach(function (content) {
+    // 每次开始解析 Markdown 内容时调用
+    wordsCount = content.match(/[^\s,\#,\*,\-,\|,\~,\[,\],\(,\),\>,\<,\!,\?]/g).length;
+    return content;
+  });
+  hook.afterEach(function (html, next) {
+    next(`<div><span style="
+    float: right;font-size: 0.9em;
+   color:rgb(90,90,90);
+">全文${wordsCount}字</span><div style="clear: both"></div></div>` + html)
+  });
+}
+
+// Docsify plugin options
+window.$docsify['countable'] = Object.assign(defaultOptions, window.$docsify['countable']);
+window.$docsify.plugins = [].concat(plugin, window.$docsify.plugins);
