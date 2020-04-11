dbf885c943e2128dfb76087fd089a032273c5a76
junking <827652549@qq.com>
2020-04-11 08:34:26 +0800

    docsify

--- dist/countable.js
+++ dist/countable.js
@@ -1,29 +1,38 @@
+//default values
 var defaultOptions = {
-  countable:true,
-  fontsize:'0.9em',
-  color:'rgb(90,90,90)',
-  language:'english'
+    countable: true,
+    fontsize: '0.9em',
+    color: 'rgb(90,90,90)',
+    language: 'english'
 };
 
 // Docsify plugin functions
 function plugin(hook, vm) {
-  let wordsCount;
-  hook.beforeEach(function (content) {
-    // 每次开始解析 Markdown 内容时调用
-    wordsCount = content.match(/([\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g).length;
-    return content;
-  });
-  hook.afterEach(function (html, next) {
-    let str = wordsCount+' words';
-    if (defaultOptions.language==='chinese'){
-      str=wordsCount+' 字';
-    }
+    let wordsCount;
+    hook.beforeEach(function (content) {
+        // Match regex every time you start parsing .md
+        wordsCount = content.match(/([\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g).length;
+        return content;
+    });
+    hook.afterEach(function (html, next) {
+        let str = wordsCount + ' words';
+        //Determine whether to use the Chinese style according to the attribute "language"
+        if (defaultOptions.language === 'chinese') {
+            str = wordsCount + ' 字';
+        }
 
-    next(`<div><span style="
-    float: right;font-size: ${defaultOptions.fontsize};
-   color:${defaultOptions.color};
-">${str}</span><div style="clear: both"></div></div>` + html)
-  });
+        //add html string
+        next(
+            `
+        <div>
+            <span style="
+                  float: right;font-size: ${defaultOptions.fontsize};
+                  color:${defaultOptions.color};">
+            ${str}
+            </span>
+            <div style="clear: both"></div>
+        </div>` + html)
+    });
 }
 
 // Docsify plugin options
