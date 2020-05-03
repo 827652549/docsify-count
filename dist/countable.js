b60ec5a4a1d40bb4207eb09ae6f8a7e9c890eef0
HerbertHe <Herbert.He0229@gmail.com>
2020-05-03 11:45:04 +0800

    feat: support localization

--- dist/countable.js
+++ dist/countable.js
@@ -7,6 +7,10 @@ var defaultOptions = {
     fontsize: "0.9em",
     color: "rgb(90,90,90)",
     language: "english",
+    localization: {
+        words: "",
+        minute: "",
+    },
     isExpected: true,
 }
 
@@ -18,16 +22,25 @@ function plugin(hook, vm) {
     let wordsCount
     hook.beforeEach(function (content) {
         // Match regex every time you start parsing .md
-        wordsCount = content.match(/([\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g).length
+        wordsCount = content.match(
+            /([\u0800-\u4e00]+?|[\u4e00-\u9fa5]+?|[a-zA-Z0-9]+)/g
+        ).length
         return content
     })
     hook.afterEach(function (html, next) {
+        // Support localization
         let str = wordsCount + " words"
         let readTime = Math.ceil(wordsCount / 400) + " min"
-        //Determine whether to use the Chinese style according to the attribute "language"
         if (defaultOptions.language === "chinese") {
             str = wordsCount + " 字"
             readTime = Math.ceil(wordsCount / 400) + " 分钟"
+        } else if (
+            defaultOptions.localization.words.length !== 0 &&
+            defaultOptions.localization.minute.length !== 0
+        ) {
+            str = wordsCount + defaultOptions.localization.words
+            readTime =
+                Math.ceil(wordsCount / 400) + defaultOptions.localization.minute
         }
 
         //add html string
